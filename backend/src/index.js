3693d1f3247a2b371b9cc41afe6421226456b981
Marco Lancellotti <marcolanciesn@gmail.com>
2019-10-17 23:02:25 +0200

    added all other apis

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,10 @@ require('../models/Proposal');
 const Proposal = mongoose.model('proposals');
 
 //#database connection
-mongoose.connect(keys.mongoUri, { useNewUrlParser: true });
+mongoose.connect(keys.mongoUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 const app = express();
 
@@ -24,34 +27,61 @@ app.use(morgan('combined'));
 
 //#routes
 // retrieve all proposals
-app.get('/', async (req, res) => {
-  const proposals = await Proposal.find({}).sort({ _id: -1 });
-  res.send(proposals);
+app.get('/proposals', async (req, res) => {
+  try {
+    const proposals = await Proposal.find({}).sort({ _id: -1 });
+    res.status(200).send(proposals);
+  } catch (e) {
+    console.error(e);
+    throw new Error(e);
+  }
 });
 
 // get a specific proposal
-app.get('/:id', (req, res) => {
-  //TODO get the proposal from db
-  const proposal = undefined;
-  if (!proposal) return res.status(404).send();
-  res.send(proposal);
+app.get('/proposals/:id', async (req, res) => {
+  try {
+    const proposal = await Proposal.findOne({ _id: req.params.id });
+    if (!proposal) return res.status(404).send();
+    res.status(200).send(proposal);
+  } catch (e) {
+    console.error(e);
+    throw new Error(e);
+  }
 });
 
 // insert a new proposal
-app.post('/', (req, res) => {
-  const { title, description } = req.body;
-  //TODO add proposal to db
-  res.status(200).send();
+app.post('/proposals', async (req, res) => {
+  const { who, what, why } = req.body;
+  try {
+    await new Proposal({
+      who,
+      why,
+      what,
+      likes: 0,
+      time: new Date(),
+      proposer: 'Anonymus'
+    }).save();
+
+    res.status(201).send();
+  } catch (e) {
+    console.error(e);
+    throw new Error(e);
+  }
 });
 
 // add like to a proposal
-app.post('/:id/like', (req, res) => {
-  //TODO get proposal from db
-  const proposal = undefined;
-  if (!proposal) return res.status(404).send();
+app.patch('/proposals/:id/like', async (req, res) => {
+  try {
+    const proposal = await Proposal.findOne({ _id: req.params.id });
+    if (!proposal) return res.status(404).send();
 
-  //TODO increment like on db
-  res.status(200).send();
+    proposal.likes += 1;
+    await proposal.save();
+    res.status(204).send();
+  } catch (e) {
+    console.error(e);
+    throw new Error(e);
+  }
 });
 
 // start the server
