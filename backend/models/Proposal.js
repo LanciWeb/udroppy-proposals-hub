30b46b0a82493a4a1b938435d353862dd91ed015
Marco Lancellotti <marcolanciesn@gmail.com>
2019-10-26 19:13:09 +0200

    fixed import for mongose comment model

--- backend/models/Proposal.js
+++ backend/models/Proposal.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
-const { CommentSchema } = require('./Comment');
+const CommentSchema = require('./Comment');
 
 const proposalSchema = new Schema({
   time: Date,
