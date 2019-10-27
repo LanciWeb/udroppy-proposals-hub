26f6708a0da8bfbece7907f21843a7e1e382e65f
Marco Lancellotti <marcolanciesn@gmail.com>
2019-10-27 12:20:36 +0100

    added style for comments

--- frontend/src/components/Comments.jsx
+++ frontend/src/components/Comments.jsx
@@ -1,29 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useAuth0 } from '../auth/Auth0Provider';
 
 const Comments = props => {
   const { comments } = props;
-
+  const { user } = useAuth0();
   const renderComments = comments =>
-    comments.map(c => (
-      <div className="comment-wrapper">
-        <div className="comment-owner">
-          <img
-            height="40"
-            alt="profile"
-            src={c.user.picture}
-            className="image-fluid mr-1"
-          />
-          {c.user.nickname} -{' '}
-          {/* <em>{c.created_at.toLocaleTimeString()}</em> */}
-        </div>
-        <div className="comment">
-          <p>{c.text}</p>
-        </div>
-      </div>
-    ));
+    comments.map(c => {
+      const dateTime = new Date(c.created_at);
+      const time = dateTime.toLocaleTimeString();
+      const date = dateTime.toLocaleDateString();
+      const directionClass =
+        user.sub === c.user.sub ? 'flex-row-reverse' : 'flex-row';
 
-  return <div id="comments-wrapper">{renderComments(comments)}</div>;
+      return (
+        <div className="comment-wrapper mb-3" key={c._id}>
+          <div className={`d-flex ${directionClass}`}>
+            <div className="comment-owner">
+              <img
+                height="40"
+                alt="profile"
+                ttle={c.user.nickname}
+                src={c.user.picture}
+                className="image-fluid "
+              />
+            </div>
+            <div className="comment p-2 mx-2 flex-grow">
+              <div>
+                <span className="nickname h6">{c.user.nickname}</span>
+              </div>
+              <p className="mb-0 d-inline">{c.text}</p>
+              <div className="timestamp">
+                {date} - {time}
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    });
+
+  return (
+    <div id="comments-wrapper" className="p-4">
+      {renderComments(comments)}
+    </div>
+  );
 };
 
 Comments.propTypes = {
@@ -31,7 +51,7 @@ Comments.propTypes = {
     PropTypes.shape({
       text: PropTypes.string.isRequired,
       user: PropTypes.object.isRequired,
-      created_at: PropTypes.instanceOf(Date)
+      created_at: PropTypes.string.isRequired
     })
   )
 };
